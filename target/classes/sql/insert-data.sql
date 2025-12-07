148d488cb4ef44b454a957e22118bd4fdbd45e1c
Jakub Wojtasik <jakubw02@protonmail.com>
2025-12-07 18:57:22 +0100

    Dodanie obiektów java typu POJO

---
+++ target/classes/sql/insert-data.sql
@@ -0,0 +1,5 @@
+sql
+
+INSERT INTO user(username, email, name, last_name) VALUES ('adam', 'adam@example.com', 'Adam', 'Kowalski');
+INSERT INTO user(username, email, name, last_name) VALUES ('ola', 'ola@example.com', 'Ola', 'Adamek');
+INSERT INTO user(username, email, name, last_name) VALUES ('jan', 'jan@example.com', 'Jan', 'Nowak');
